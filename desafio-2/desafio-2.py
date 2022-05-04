2cbdfcfdea8c9f1247229918782d79826dba31e3
Henrique Eissmann Buzzi <71103223+h-buzzi@users.noreply.github.com>
2022-05-04 17:13:06 -0300

    updated code comments

--- desafio-2/desafio-2.py
+++ desafio-2/desafio-2.py
@@ -4,10 +4,11 @@ Created on Mon May  2 17:53:15 2022
 
 @author: henrique buzzi
 """
-
+## Entradas
 x = 3 #Número de alunos mínimos presentes no horário da aula
 tempoChegada = [-2,-1,0,1,2] #Vetor com os tempos de chegada de cada aluno
 
+##Processamento
 for chegada in tempoChegada: #Loop que passa por cada tempo de chegada
     if(chegada>=0): #Se o tempo de chegada for maior ou igual a zero, o aluno estava presente no horário de começar a aula
         x-=1 #Diminui em 1 o número de alunos necessários para garantir a Aula normal
