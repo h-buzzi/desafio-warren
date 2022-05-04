c672719f80a446dd9e528e859a8e1326748ae350
Henrique Eissmann Buzzi <71103223+h-buzzi@users.noreply.github.com>
2022-05-04 16:34:57 -0300

    Add files via upload

---
+++ desafio-2/desafio-2.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May  2 17:53:15 2022
+
+@author: henrique buzzi
+"""
+
+x = 3 #Número de alunos mínimos presentes no horário da aula
+tempoChegada = [-2,-1,0,1,2] #Vetor com os tempos de chegada de cada aluno
+
+for chegada in tempoChegada: #Loop que passa por cada tempo de chegada
+    if(chegada>=0): #Se o tempo de chegada for maior ou igual a zero, o aluno estava presente no horário de começar a aula
+        x-=1 #Diminui em 1 o número de alunos necessários para garantir a Aula normal
+
+if(x<=0): #Se o número de alunos necessários tiver sido atingido ou até superado
+    print('Aula Normal') #Aula normal
+else: #Se não atingiu o número necessário
+    print('Aula Cancelada') #Aula cancelada
